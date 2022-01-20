9f14aed7ac067ece31633a84cfd4d5239fe2d229
Jakub Nowocień <jakub_nowocien47@wp.pl>
2022-01-20 19:39:30 +0100

    rozpoczęcie prac

--- TicTacToe/main.py
+++ TicTacToe/main.py
@@ -1,1 +1,14 @@
 print("Hello world")
+
+class Stan:
+    '''
+    Klasa przechowująca stany
+    '''
+    def __init__(self):
+        self.poprzedni = 0
+def Szukaj():
+    '''
+    Główna funkcja programu
+    :return:
+    '''
+    pass
